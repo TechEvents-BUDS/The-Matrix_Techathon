ce9e2a154884eae491378da2853c4afdd771ea4e
Danish <danishsidd524@gmail.com>
2024-12-18 16:23:03 +0500

    fix image

--- client/components/shared/Header.tsx
+++ client/components/shared/Header.tsx
@@ -4,9 +4,7 @@ import { ProfileButton } from "./ProfileButton";
 export const Header = () => {
   return (
     <header className="flex justify-between items-center p-4 bg-neutral-200 shadow-md">
-      <div>
-        <img src="/images/bg.jpg"/>
-      </div>
+      <div></div>
       <div>
         <ProfileButton />
       </div>
